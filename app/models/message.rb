22a174838f30120c26d515a7db349f321d4a89ca
Alpha7 <henryomondi6612@gmail.com>
2023-10-21 10:44:49 +0300

    Add linters

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,12 +1,11 @@
 class Greeting < ApplicationRecord
-    GREETINGS = [
-      'Hello, how are you?',
-      'Good morning!',
-      'Good afternoon!',
-      'Good evening!',
-      'Welcome!'
-    ].freeze
-  
-    validates :content, presence: true
-  end
-  
+  GREETINGS = [
+    'Hello, how are you?',
+    'Good morning!',
+    'Good afternoon!',
+    'Good evening!',
+    'Welcome!'
+  ].freeze
+
+  validates :content, presence: true
+end
