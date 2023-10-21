334a5075d7d47da4d0513a0917099d2a743b59e1
Alpha7 <henryomondi6612@gmail.com>
2023-10-21 22:49:19 +0300

    Add changes requested by the reviewer

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  get 'api/greetings', to: 'greetings#index'  # Adjust this route to match your desired controller and action
+  root 'root#index'
+
+  namespace :api do
+    resources :greetings
+  end
 end
